853058c7244737ebf60d496bdfba72f0276e420e
lonnwolf031 <lonn.wolf031@gmail.com>
2021-09-17 17:13:33 +0200

    Initial commit

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Obstacle : MonoBehaviour {
+
+    //box and barrel found here: https://www.assetstore.unity3d.com/en/#!/content/11256
+
+    void OnTriggerEnter(Collider col)
+    {
+        //if the player hits one obstacle, it's game over
+        if(col.gameObject.tag == Constants.PlayerTag)
+        {
+            GameManager.Instance.Die();
+        }
+    }
+}
